111bd97f568fb1f4220b37a11fc619e86cd55354
jigar <jigshahpuzzle@gmail.com>
2017-05-20 17:22:32 -0500

    added movement

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import pygame, sys
 from pygame.locals import *
 
 def executable():
-	env = PongGameEnv(400, 400)
-	env.startup()
+	width = 400
+	height = 400
+	fps = 60
+	env = PongGameEnv(width, height, fps)
+	env.run_game()
 
 class PongGameEnv(object):
 
-	def __init__(self, width, height):
-		self.fps = 200
+	def __init__(self, width, height, fps):
+		self.fps = fps
 		self.width = width
 		self.height = height
 		self.colors = Colors()
@@ -16,18 +19,21 @@ class PongGameEnv(object):
 	def draw_bg(self):
 		self.display.fill(self.colors.maroon)
 
-	def startup(self):
+	def run_game(self):
 		pygame.init()
 		self.clock = pygame.time.Clock()
 		self.display = pygame.display.set_mode((self.width, self.height))
-		self.draw_bg()
-		paddle = Paddle()
-		paddle.draw_paddle(self.display)
+		paddle = Paddle(self.width, self.height)
 		while True:
 			for event in pygame.event.get():
 				if event.type == QUIT:
 					pygame.quit()
 					sys.exit()
+			keys=pygame.key.get_pressed()
+			if keys[K_LEFT]:
+				paddle.move_left()
+			if keys[K_RIGHT]:
+				paddle.move_right()
 			self.draw_bg()
 			paddle.draw_paddle(self.display)
 			pygame.display.update()
@@ -35,17 +41,23 @@ class PongGameEnv(object):
 
 class Paddle(object):
 
-	def __init__(self):
-		self.xpos = 200
-		self.ypos = 370
-		self.width = 50
-		self.height = 50
+	def __init__(self, game_width, game_height):
+		self.xpos = game_width / 2
+		self.ypos = game_height - 30
+		self.width = game_width / 8
+		self.length = 4
 		self.colors = Colors()
 
 	def draw_paddle(self, display):
-		paddle = pygame.Rect(self.xpos, self.ypos, self.width, 4)
+		paddle = pygame.Rect(self.xpos, self.ypos, self.width, self.length)
 		pygame.draw.rect(display, self.colors.black, paddle)
 		pass
+
+	def move_left(self):
+		self.xpos += -2
+
+	def move_right(self):
+		self.xpos += 2
 
 class Brick(object):
 
